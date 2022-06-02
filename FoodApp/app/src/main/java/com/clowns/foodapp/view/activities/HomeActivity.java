242e4495defd63e4c8f2a7ed19ed4da035232934
Tran Tan Phat <kashukiller@gmai.com>
2022-06-02 14:02:32 +0700

    PhatTT push 02/06/2022

--- FoodApp/app/src/main/java/com/clowns/foodapp/view/activities/HomeActivity.java
+++ FoodApp/app/src/main/java/com/clowns/foodapp/view/activities/HomeActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 
 import android.annotation.SuppressLint;
@@ -12,14 +13,60 @@ import android.view.MenuItem;
 import android.view.View;
 
 import com.clowns.foodapp.R;
+import com.clowns.foodapp.databinding.ActivityHomeBinding;
+import com.clowns.foodapp.view.cart.MyCartFragment;
+import com.clowns.foodapp.view.cart.MyOrdersFragment;
+import com.clowns.foodapp.view.home.MainFragment;
+import com.clowns.foodapp.view.others.MyFavouriteFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class HomeActivity extends AppCompatActivity {
 
+    ActivityHomeBinding binding;
+
+    // Edited by PhatTT
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
+        binding.bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.home_page:
+                        fragment = new MainFragment();
+                        loadFragment(fragment);
+                        return true;
+                    case R.id.favourite_page:
+                        fragment = new MyFavouriteFragment();
+                        loadFragment(fragment);
+                        return true;
+                    case R.id.cart_page:
+                        fragment = new MyCartFragment();
+                        loadFragment(fragment);
+                        return true;
+                    case R.id.order_page:
+                        fragment = new MyOrdersFragment();
+                        loadFragment(fragment);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
+
+    // Created by PhatTT
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragments, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
 }
--- FoodApp/app/src/main/java/com/clowns/foodapp/view/others/MyProfileFragment.java
+++ FoodApp/app/src/main/java/com/clowns/foodapp/view/others/MyProfileFragment.java
@@ -1,7 +1,11 @@
 package com.clowns.foodapp.view.others;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,58 +13,47 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.clowns.foodapp.R;
+import com.clowns.foodapp.databinding.FragmentMyProfileBinding;
+import com.clowns.foodapp.model.User;
+import com.clowns.foodapp.view.activities.HomeActivity;
+import com.clowns.foodapp.view.activities.WelcomeActivity;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MyProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MyProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MyProfileFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MyProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MyProfileFragment newInstance(String param1, String param2) {
-        MyProfileFragment fragment = new MyProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    FragmentMyProfileBinding binding;
+    User user;
+    // Edited by PhatTT
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            user = (User) getArguments().getSerializable("user");
         }
     }
 
+    // Edited by PhatTT
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_profile, container, false);
+        binding = DataBindingUtil.inflate(getLayoutInflater(), R.layout.fragment_my_profile, null, false);
+        View viewRoot = binding.getRoot();
+        binding.setUser(user);
+        return viewRoot;
+    }
+
+    // Created by PhatTT
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.signOutEfab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), WelcomeActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
     }
 }
