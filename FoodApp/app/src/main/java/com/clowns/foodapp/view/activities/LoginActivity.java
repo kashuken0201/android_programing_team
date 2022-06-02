c9082c2cdce665cbd805ccf746ed068c7dc1de3f
daiphuoc511 <duongdaiphuoc511@gmail.com>
2022-06-02 23:12:47 +0700

    PhuocDD push 2/6/2022

--- FoodApp/app/src/main/java/com/clowns/foodapp/view/activities/LoginActivity.java
+++ FoodApp/app/src/main/java/com/clowns/foodapp/view/activities/LoginActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.clowns.foodapp.R;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,5 +14,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
     }
 }
