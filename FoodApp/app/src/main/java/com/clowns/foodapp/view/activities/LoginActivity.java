eee49d688adf329452d00bb80d306c32f3003f16
daiphuoc511 <duongdaiphuoc511@gmail.com>
2022-06-26 11:58:58 +0700

    PhuocDD push 26/6/2022

--- FoodApp/app/src/main/java/com/clowns/foodapp/view/activities/LoginActivity.java
+++ FoodApp/app/src/main/java/com/clowns/foodapp/view/activities/LoginActivity.java
@@ -6,6 +6,7 @@ import androidx.databinding.DataBindingUtil;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -13,13 +14,13 @@ import com.clowns.foodapp.R;
 import com.clowns.foodapp.databinding.ActivityLoginBinding;
 import com.clowns.foodapp.view.home.MainFragment;
 import com.clowns.foodapp.viewmodel.adapters.LoginAdapter;
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.identity.SignInCredential;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
@@ -38,9 +39,10 @@ public class LoginActivity extends AppCompatActivity {
     ActivityLoginBinding binding;
 
     private FirebaseAuth mAuth;
-    GoogleSignInOptions googleSignInOptions;
+    BeginSignInRequest signInRequest;
     GoogleSignInClient googleSignInClient;
-    private final static int RC_SIGN_IN = 100;
+    private final static int REQ_ONE_TAP = 100;
+    private boolean showOneTapUI = true;
 
     // PhuocDD
     @Override
@@ -48,10 +50,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onStart();
 
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            Intent intent = new Intent(getApplicationContext(), MainFragment.class);
-            startActivity(intent);
-        }
+        updateUI(currentUser);
     }
 
     @Override
@@ -64,15 +63,20 @@ public class LoginActivity extends AppCompatActivity {
         // PhuocDD
         mAuth = FirebaseAuth.getInstance();
 
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        AppEventsLogger.activateApp(this);
-
-        createSignInGoogle();
+        signInRequest = BeginSignInRequest.builder()
+                .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                        .setSupported(true)
+                        // Your server's client ID, not your Android client ID.
+                        .setServerClientId(getString(R.string.default_client_id))
+                        .setFilterByAuthorizedAccounts(true)
+                        .build())
+                .build();
 
         // PhuocDD
         binding.ggLoginLoginMb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Toast.makeText(LoginActivity.this,"TAO LAY MAY",Toast.LENGTH_LONG).show();
                 signInWithGoogle();
             }
         });
@@ -101,16 +105,9 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     // PhuocDD
-    private void createSignInGoogle() {
-        googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail().build();
-        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
-    }
-
-    // PhuocDD
     private void signInWithGoogle() {
         Intent signInIntent = googleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
+        startActivityIfNeeded(signInIntent, REQ_ONE_TAP);
     }
 
     // PhuocDD
@@ -118,15 +115,70 @@ public class LoginActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
+        switch (requestCode) {
+            case REQ_ONE_TAP:
+                try {
+                    SignInClient oneTapClient = null;
+                    SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(data);
+                    String idToken = credential.getGoogleIdToken();
+                    if (idToken !=  null) {
+                        // Got an ID token from Google. Use it to authenticate
+                        // with Firebase.
+                        Toast.makeText(this,"Got ID tonken",Toast.LENGTH_LONG).show();
+                    }
+                } catch (ApiException e) {
+                    switch (e.getStatusCode()) {
+                        case CommonStatusCodes.CANCELED:
+                            Log.e("Error", "One-tap dialog was closed.");
+                            Toast.makeText(this,"ne-tap dialog was closed.",Toast.LENGTH_LONG).show();
+                            // Don't re-prompt the user.
+                            showOneTapUI = false;
+                            break;
+                        case CommonStatusCodes.NETWORK_ERROR:
+                            Log.e("Error", "One-tap encountered a network error.");
+                            Toast.makeText(this,"One-tap encountered a network error.",Toast.LENGTH_LONG).show();
+                            // Try again or just ignore.
+                            break;
+                        default:
+                            Log.e("Error", "Couldn't get credential from result."
+                                    + e.getLocalizedMessage());
+                            Toast.makeText(this,"Couldn't get credential from result.",Toast.LENGTH_LONG).show();
+                            break;
+                    }
+                }
+                break;
+        }
 
-        if(requestCode == RC_SIGN_IN) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            try {
-                GoogleSignInAccount account = task.getResult(ApiException.class);
-                firebaseAuthWithGoogle(account);
-            } catch (ApiException e) {
-                Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
-            }
+        SignInClient oneTapClient = null;
+        SignInCredential googleCredential = null;
+        try {
+            googleCredential = oneTapClient.getSignInCredentialFromIntent(data);
+        } catch (ApiException e) {
+            e.printStackTrace();
+        }
+        String idToken = googleCredential.getGoogleIdToken();
+        if (idToken !=  null) {
+            // Got an ID token from Google. Use it to authenticate
+            // with Firebase.
+            AuthCredential firebaseCredential = GoogleAuthProvider.getCredential(idToken, null);
+            mAuth.signInWithCredential(firebaseCredential)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.e("Error", "signInWithCredential:success");
+                                Toast.makeText(LoginActivity.this,"TAO LAY MAY",Toast.LENGTH_LONG).show();
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w("Error", "signInWithCredential:failure", task.getException());
+                                Toast.makeText(LoginActivity.this,"TAO LAY MAY",Toast.LENGTH_LONG).show();
+                                updateUI(null);
+                            }
+                        }
+                    });
         }
     }
 
@@ -146,5 +198,17 @@ public class LoginActivity extends AppCompatActivity {
                         }
                     }
                 });
+    }
+
+    public void updateUI(FirebaseUser account){
+
+        if(account != null){
+            Toast.makeText(this,"You Signed In successfully",Toast.LENGTH_LONG).show();
+            startActivity(new Intent(getApplicationContext(),MainFragment.class));
+
+        }else {
+            Toast.makeText(this,"You Didnt signed in",Toast.LENGTH_LONG).show();
+        }
+
     }
 }
